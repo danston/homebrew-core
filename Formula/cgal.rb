c1752cb8866528bb436501d8294a85d21bf02e30
Dmitry Anisimov <danston@ymail.com>
2019-07-03 13:23:14 +0200

    fixed cgal desc typo

--- Formula/cgal.rb
+++ Formula/cgal.rb
@@ -1,5 +1,5 @@
 class Cgal < Formula
-  desc "Computational Geometry Algorithm Library"
+  desc "Computational Geometry Algorithms Library"
   homepage "https://www.cgal.org/"
   url "https://github.com/CGAL/cgal/releases/download/releases%2FCGAL-4.14/CGAL-4.14.tar.xz"
   sha256 "59464b1eaee892f2223ba570a7642892c999e29524ab102a6efd7c29c94a29f7"
